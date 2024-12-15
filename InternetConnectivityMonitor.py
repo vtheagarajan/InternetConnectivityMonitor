a5fc5a17bd07eb4201aef2c9deb1baecb6f50141
Vaidhe Theagarajan <vt19912016@gmail.com>
2024-12-15 10:07:22 -0600

    Added error handling

--- InternetConnectivityMonitor.py
+++ InternetConnectivityMonitor.py
@@ -25,6 +25,7 @@ SMTP_SERVER = os.environ.get("SMTP_SERVER")
 SMTP_PORT = os.environ.get("SMTP_PORT")
 OUTAGE_LOG_FILE = "./Logs/outage_log.csv"  # Path to CSV file
 CONNECTION_LOG_FILE = "./Logs/connection_log.csv"
+ERROR_LOG_FILE = "./Logs/error_log.txt"
 SIMULATE_FAILURE_FOR_TESTING = False
 DAILY_REPORT_TIME = os.environ.get("DAILY_REPORT_TIME")
 
@@ -78,6 +79,7 @@ def log_outage_to_csv(start, end, duration):
 
     except Exception as e:
         print(f"Failed to write to CSV: {e}")
+        raise Exception("Failed to write to outage log: {e}")
 
 def log_connection_to_csv(log_time, isConnected):
     try:
@@ -89,6 +91,16 @@ def log_connection_to_csv(log_time, isConnected):
 
     except:
         print("Failed to write to connection log")
+        raise Exception("Failed to write to connection log: {e}")
+
+def log_error_to_file(error_message):
+    errlogentry = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {error_message}'
+                          
+    try:
+        with open(ERROR_LOG_FILE, mode='a') as file:
+            file.write(errlogentry + '\n')
+    except:
+        print("Failed to write to error log")
 
 #Funtion to get the number of checks performed over the last 24 hours from connection_log.csv
 # Also return the outage records from outage_log.csv during that time
@@ -121,65 +133,71 @@ def getLast24HrReport():
 def main():
     global outage_start, outage_reported
 
-    # Main loop
-    while True:
-        #DAILY_REPORT_TIME format will '%H:%M:%S'. Need to append this time value to the current date 
-        # to get the full date-time for current date
-        # Get current date
-        current_date = datetime.now().date()
+    try:
+        # Main loop
+        while True:
+            #DAILY_REPORT_TIME format will '%H:%M:%S'. Need to append this time value to the current date 
+            # to get the full date-time for current date
+            # Get current date
+            current_date = datetime.now().date()
 
-        # Parse the time from environment variable with seconds included
-        daily_report_time = datetime.strptime(DAILY_REPORT_TIME, '%H:%M:%S').time()
+            # Parse the time from environment variable with seconds included
+            daily_report_time = datetime.strptime(DAILY_REPORT_TIME, '%H:%M:%S').time()
 
-        # Combine current date with the time from environment variable
-        daily_report_datetime = datetime.combine(current_date, daily_report_time)
-        
-        #get the absolute value of the time difference in seconds
-        diff_secs = abs((datetime.now() - daily_report_datetime).total_seconds())
-        print(f"Seconds away from report time: {diff_secs}")
+            # Combine current date with the time from environment variable
+            daily_report_datetime = datetime.combine(current_date, daily_report_time)
+            
+            #get the absolute value of the time difference in seconds
+            diff_secs = abs((datetime.now() - daily_report_datetime).total_seconds())
+            print(f"Seconds away from report time: {diff_secs}")
 
-        #check if the current time is within CHECK_INTERVAL seonds from the environment variable for DAILY_REPORT_TIME
-        if diff_secs <= CHECK_INTERVAL:
-            print(f"Daily report time reached, so sening daily report emaill")
-            #get the number of checks performed over the last 24 hours
-            checks_last_24_hours, outage_records = getLast24HrReport()
-            #send email with the number of checks performed over the last 24 hours
-            send_email(
-                "Daily Report",
-                f"Number of checks performed over the last 24 hours: {checks_last_24_hours}\n"
-                f"Outage records: {outage_records}"
-            )
-        
-        isConnected = is_connected()
-        if isConnected:
-            if outage_reported:
-                # Outage ended, send email and log details
-                outage_end = datetime.now()
-                outage_duration = (outage_end - outage_start).total_seconds()
+            #check if the current time is within CHECK_INTERVAL seonds from the environment variable for DAILY_REPORT_TIME
+            if diff_secs <= CHECK_INTERVAL:
+                print(f"Daily report time reached, so sening daily report emaill")
+                #get the number of checks performed over the last 24 hours
+                checks_last_24_hours, outage_records = getLast24HrReport()
+                #send email with the number of checks performed over the last 24 hours
                 send_email(
-                    "Internet Restored",
-                    f"Internet was down from {outage_start} to {outage_end}.\n"
-                    f"Total downtime: {outage_duration:.2f} seconds."
+                    "Daily Report",
+                    f"Number of checks performed over the last 24 hours: {checks_last_24_hours}\n"
+                    f"Outage records: {outage_records}"
                 )
-                log_outage_to_csv(outage_start, outage_end, outage_duration)
-                outage_start = None
-                outage_reported = False
-        else:
-            if not outage_reported:
-                # Outage started, send email
-                outage_start = datetime.now()
-                
-                #No point in trying to send email if the connectivity is down - it will not go through anyway
-                #send_email(
-                #    "Internet Outage Detected",
-                #    f"Internet connectivity lost at {outage_start}."
-                #)
-                outage_reported = True
+            
+            isConnected = is_connected()
+            if isConnected:
+                if outage_reported:
+                    # Outage ended, send email and log details
+                    outage_end = datetime.now()
+                    outage_duration = (outage_end - outage_start).total_seconds()
+                    send_email(
+                        "Internet Restored",
+                        f"Internet was down from {outage_start} to {outage_end}.\n"
+                        f"Total downtime: {outage_duration:.2f} seconds."
+                    )
+                    log_outage_to_csv(outage_start, outage_end, outage_duration)
+                    outage_start = None
+                    outage_reported = False
+            else:
+                if not outage_reported:
+                    # Outage started, send email
+                    outage_start = datetime.now()
+                    
+                    #No point in trying to send email if the connectivity is down - it will not go through anyway
+                    #send_email(
+                    #    "Internet Outage Detected",
+                    #    f"Internet connectivity lost at {outage_start}."
+                    #)
+                    outage_reported = True
 
-        log_connection_to_csv(datetime.now(), isConnected)
-        print(f"Connection status at {datetime.now()}: {isConnected}")
+            log_connection_to_csv(datetime.now(), isConnected)
+            print(f"Connection status at {datetime.now()}: {isConnected}")
 
-        sleep(CHECK_INTERVAL)
+            sleep(CHECK_INTERVAL)
+    except Exception as ex:
+        print(f"An error occurred: {ex}")
+        log_error_to_file(ex)
+        send_email("Internet Connectivity Monitor Error", f"An error occurred: {ex}")
+
 
 
 if __name__ == "__main__":
